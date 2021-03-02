53a090b8c7b306967d9d46f6f049ef46aec70554
Esther Nampiina <nay.esther08@gmail.com>
2021-03-02 22:41:26 +0100

    Shopping Cart components added

---
+++ EssyWigs/Components/ShoppingCartSummary.cs
@@ -0,0 +1,31 @@
+﻿using EssyWigs.Models;
+using EssyWigs.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EssyWigs.Components
+{
+    public class ShoppingCartSummary: ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartProducts();
+            _shoppingCart.ShoppingCartProducts = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
---
+++ EssyWigs/Components/SupplierMenu.cs
@@ -0,0 +1,25 @@
+﻿using EssyWigs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EssyWigs.Components
+{
+    public class SupplierMenu: ViewComponent
+    {
+        private readonly ISupplierRepository _supplierRepository;
+        
+
+        public SupplierMenu(ISupplierRepository supplierRepository)
+        {
+            _supplierRepository = supplierRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var suppliers = _supplierRepository.AllSuppliers.OrderBy(s => s.SupplierName);
+            return View(suppliers);
+        }
+    }
+}
