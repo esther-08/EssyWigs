0cbc144762ffd231555762c88d7da25f65f5b8be
Esther Nampiina <nay.esther08@gmail.com>
2021-03-05 15:40:11 +0100

    Page edited

--- EssyWigs/Migrations/20210305135434_IdentityAdded.Designer.cs
+++ EssyWigs/Migrations/20210305135434_IdentityAdded.Designer.cs
@@ -4,14 +4,16 @@ using EssyWigs.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EssyWigs.Migrations
 {
     [DbContext(typeof(WigDbContext))]
-    partial class WigDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210305135434_IdentityAdded")]
+    partial class IdentityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
